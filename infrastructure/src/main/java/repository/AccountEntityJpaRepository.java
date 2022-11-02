98b06d475b2dc70d1c34496089a0043ec6397720
Ismail <elbadre.ib@gmail.com>
2022-11-02 14:21:37 +0100

    feat(infrastructure): adding AccountEntityJpaRepository

---
+++ infrastructure/src/main/java/repository/AccountEntityJpaRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+import org.exalt.entities.AccountEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AccountEntityJpaRepository extends JpaRepository<AccountEntity, UUID> {
+
+}
