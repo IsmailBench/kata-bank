c3b1580e88e4b33d13ecee4c520315b6aaa00e73
Ismail <elbadre.ib@gmail.com>
2022-11-02 11:14:35 +0100

    test(domain): refactoring test and adding import

--- domain/src/test/java/org/exalt/model/AccountImplTest.java
+++ domain/src/test/java/org/exalt/model/AccountImplTest.java
@@ -1,6 +1,10 @@
 package org.exalt.model;
 
 
+import org.exalt.exceptions.AmountGreaterThanBalanceInTransactionException;
+import org.exalt.exceptions.NegativeAmountInTransactionException;
+import org.exalt.model.impl.AccountImpl;
+import org.exalt.model.intf.Account;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -18,39 +22,51 @@ public class AccountImplTest {
     @Test
     public void should_not_withdraw_when_amount_is_greater_than_balance() {
         UUID id = UUID.randomUUID();
-        AccountImpl accountImpl = new AccountImpl(id, "Paul", new BigDecimal(1000), Instant.now(), new HashSet<>());
-        assertThrows(AmountGreaterThanBalanceInTransactionException.class , () -> accountImpl.withdraw(new BigDecimal(50000)));
+        Account account = new AccountImpl(id, "Paul", new BigDecimal(1000), Instant.now(), new HashSet<>());
+        assertThrows(AmountGreaterThanBalanceInTransactionException.class , () -> account.withdraw(new BigDecimal(50000)));
     }
 
     @Test
     public void should_not_withdraw_when_amount_is_negative() {
         UUID id = UUID.randomUUID();
-        AccountImpl accountImpl = new AccountImpl(id, "Paul", new BigDecimal(1000), Instant.now(), new HashSet<>());
-        assertThrows(NegativeAmountInTransactionException.class , () -> accountImpl.withdraw(new BigDecimal(-50000)));
+        Account account = new AccountImpl(id, "Paul", new BigDecimal(1000), Instant.now(), new HashSet<>());
+        assertThrows(NegativeAmountInTransactionException.class , () -> account.withdraw(new BigDecimal(-50000)));
     }
 
     @Test
-    public void should_not_depoosit_when_amount_is_negative() {
+    public void should_not_withdraw_when_amount_is_zero() {
         UUID id = UUID.randomUUID();
-        AccountImpl accountImpl = new AccountImpl(id, "Paul", new BigDecimal(1000), Instant.now(), new HashSet<>());
-        assertThrows(NegativeAmountInTransactionException.class , () -> accountImpl.deposit(new BigDecimal(-50000)));
+        Account account = new AccountImpl(id, "Paul", new BigDecimal(1000), Instant.now(), new HashSet<>());
+        assertThrows(NegativeAmountInTransactionException.class , () -> account.withdraw(new BigDecimal(0)));
+    }
+
+    @Test
+    public void should_not_deposit_when_amount_is_zero() {
+        UUID id = UUID.randomUUID();
+        Account account = new AccountImpl(id, "Paul", new BigDecimal(1000), Instant.now(), new HashSet<>());
+        assertThrows(NegativeAmountInTransactionException.class , () -> account.deposit(new BigDecimal(0)));
+    }
+    @Test
+    public void should_not_deposit_when_amount_is_negative() {
+        UUID id = UUID.randomUUID();
+        Account account = new AccountImpl(id, "Paul", new BigDecimal(1000), Instant.now(), new HashSet<>());
+        assertThrows(NegativeAmountInTransactionException.class , () -> account.deposit(new BigDecimal(-50000)));
     }
 
     @Test
     public void should_withdraw() {
         UUID id = UUID.randomUUID();
-        AccountImpl accountImpl = new AccountImpl(id, "Paul", new BigDecimal(1000), Instant.now(), new HashSet<>());
-        accountImpl.withdraw(new BigDecimal(500));
-        assertEquals(accountImpl.getBalance(), new BigDecimal(500));
-
+        Account account = new AccountImpl(id, "Paul", new BigDecimal(1000), Instant.now(), new HashSet<>());
+        account.withdraw(new BigDecimal(500));
+        assertEquals(account.getBalance(), new BigDecimal(500));
     }
 
     @Test
     public void should_deposit() {
         UUID id = UUID.randomUUID();
-        AccountImpl accountImpl = new AccountImpl(id, "Paul", new BigDecimal(1000), Instant.now(), new HashSet<>());
-        accountImpl.deposit(new BigDecimal(500));
-        assertEquals(accountImpl.getBalance(), new BigDecimal(1500));
+        Account account = new AccountImpl(id, "Paul", new BigDecimal(1000), Instant.now(), new HashSet<>());
+        account.deposit(new BigDecimal(500));
+        assertEquals(account.getBalance(), new BigDecimal(1500));
     }
 
 }
