903d47a7ff14c342dbdb039588207ab3063aed24
Ismail <elbadre.ib@gmail.com>
2022-11-02 17:37:31 +0100

    refactor(application): removing unused import

--- application/src/main/java/org/exalt/configuration/AccountConfig.java
+++ application/src/main/java/org/exalt/configuration/AccountConfig.java
@@ -1,12 +1,9 @@
 package org.exalt.configuration;
 
-import org.exalt.adapters.AccountJpaAdapter;
 import org.modelmapper.ModelMapper;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.exalt.repository.AccountEntityJpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @Configuration
